72ff5d65b2a486aaf5287ce7dfff36fdfc14b1d4
mattatttaq <mattatttaq@gmail.com>
2017-10-16 15:09:35 -0400

    Updating gh-pages
    Added Router

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,6 @@ import { sayHi } from './message';
 import App from './App.jsx';
 import { NavBar } from './NavBar/NavBar.jsx';
 import {Dashboard } from './Dashboard/Dashboard.jsx';
+import { Card } from './Card/Card.jsx';
+import { Deck } from './Deck/Deck.jsx';
 const css = require('../sass/main.sass');
