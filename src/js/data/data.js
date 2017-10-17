4daf81dd49bc6bac808279b32ae75091276aba74
mattatttaq <mattatttaq@gmail.com>
2017-10-17 17:37:27 -0400

    Interim checkin
    Added data, rendering out a card that i'm going to iterate over. Then once I get the card looking alright i'm going to work on the deck page.
    
    First is showing all the cards, then adding sort, then adding the ability to change the json. Then i'll add images for the real data. Last I'll work on the user object and their card.

---
+++ src/js/data/data.js
@@ -0,0 +1,40 @@
+export default {
+    "user": {
+        "f_name":"Matthew",
+        "l_name":"Allbright"
+        },
+    "cards": [
+        {
+          "c_name":"angie's list",
+          "full_name":"Person McBodyman",
+          "url":"http://www.angieslist.com",
+          "img_front": "https://picsum.photos/400",
+          "flipped": true,
+          "met":""
+        },
+        {
+          "c_name":"Blocko Taco",
+          "full_name":"Person McBodyman",
+          "url":"http://www.blocksfortacs.com",
+          "img_front": "https://picsum.photos/400",
+          "flipped": true,
+          "met":""
+        },
+        {
+          "c_name":"Strawberry Fields Managment Group",
+          "full_name":"Person McBodyman",
+          "url":"http://www.drippytrippy.com",
+          "img_front":"https://picsum.photos/400",
+          "flipped": false,
+          "met":""
+        },
+          {
+          "c_name":"Plumb Luck",
+          "full_name":"Person McBodyman",
+          "url":"http://www.plumboutofluck.com",
+          "img_front":"https://picsum.photos/400",
+          "flipped": true,
+          "met":""
+        },
+      ]
+}
